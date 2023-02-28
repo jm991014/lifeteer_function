8d3a372ada2132bf6c6ff819f4bd8860973ad873
Jong Myoung <jm991014@gmail.com>
2023-02-28 14:44:31 +0900

    .
    .

--- lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_StartPage/SplashPage.kt
+++ lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_StartPage/SplashPage.kt
@@ -1,7 +1,8 @@
-package com.example.lifeteer_ex1.LandingPage
+package com.example.lifeteer_ex1.A_StartPage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lifeteer_ex1.R
 
 class SplashPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
