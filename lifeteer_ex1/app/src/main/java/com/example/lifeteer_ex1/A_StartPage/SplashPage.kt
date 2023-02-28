c5bd59c43c8b1f64dc0a4fe2275baa9c695fca75
Jong Myoung <jm991014@gmail.com>
2023-02-28 16:02:42 +0900

    some changes
    some changes

--- lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_StartPage/SplashPage.kt
+++ lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_StartPage/SplashPage.kt
@@ -1,12 +1,23 @@
 package com.example.lifeteer_ex1.A_StartPage
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.example.lifeteer_ex1.R
+import com.example.lifeteer_ex1.databinding.ActivitySplashPageBinding
 
 class SplashPage : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_page)
+        window.statusBarColor = ContextCompat.getColor(
+            this, R.color.mindset_main_background)
+        binding = ActivitySplashPageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
