2a6f5eac4d31ea128dd53f6415ef545fae7a8dd3
Jong Myoung <jm991014@gmail.com>
2023-03-02 20:22:20 +0900

    Api 붙이기 연습
    연습

---
+++ kotlin_API1/app/src/main/java/com/example/kotlin_api1/EmgMedResponse.kt
@@ -0,0 +1,57 @@
+package com.example.kotlin_api1
+
+
+import com.squareup.moshi.Json
+
+data class EmgMedResponse(
+    @field:Json(name = "EmgMedInfo")
+    val emgMedInfo: List<EmgMedInfo?>?
+)
+
+data class EmgMedInfo(
+    @field:Json(name = "head")
+    val head: List<Head?>?,
+    @field:Json(name = "row")
+    val row: List<Row?>?
+)
+
+data class Head(
+    @field:Json(name = "api_version")
+    val apiVersion: String?,
+    @field:Json(name = "list_total_count")
+    val listTotalCount: Int?,
+    @field:Json(name = "RESULT")
+    val rESULT: RESULT?
+)
+
+data class RESULT(
+    @field:Json(name = "CODE")
+    val cODE: String?,
+    @field:Json(name = "MESSAGE")
+    val mESSAGE: String?
+)
+
+data class Row(
+    @field:Json(name = "DISTRCT_DIV_NM")
+    val dISTRCTDIVNM: String?,
+    @field:Json(name = "EMGNCY_CENTER_TELNO")
+    val eMGNCYCENTERTELNO: String?,
+    @field:Json(name = "MEDCARE_INST_NM")
+    val mEDCAREINSTNM: String?,
+    @field:Json(name = "REFINE_LOTNO_ADDR")
+    val rEFINELOTNOADDR: String?,
+    @field:Json(name = "REFINE_ROADNM_ADDR")
+    val rEFINEROADNMADDR: String?,
+    @field:Json(name = "REFINE_WGS84_LAT")
+    val rEFINEWGS84LAT: String?,
+    @field:Json(name = "REFINE_WGS84_LOGT")
+    val rEFINEWGS84LOGT: String?,
+    @field:Json(name = "REPRSNT_TELNO")
+    val rEPRSNTTELNO: String?,
+    @field:Json(name = "SIGUN_CD")
+    val sIGUNCD: String?,
+    @field:Json(name = "SIGUN_NM")
+    val sIGUNNM: String?,
+    @field:Json(name = "SUM_DE")
+    val sUMDE: String?
+)
