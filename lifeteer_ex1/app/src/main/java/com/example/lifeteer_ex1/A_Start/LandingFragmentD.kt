fd769f5df694ff521004f1ad399670f8930ef428
Jong <jm991014@gmail.com>
2023-03-06 23:20:04 +0900

    fragment to activity problem
    나중에 다시 찾아보고 마무리

--- lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_Start/LandingFragmentD.kt
+++ lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_Start/LandingFragmentD.kt
@@ -7,17 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lifeteer_ex1.R
+import com.example.lifeteer_ex1.databinding.ActivityLoginBinding
+import com.example.lifeteer_ex1.databinding.ActivityMainBinding
 import com.example.lifeteer_ex1.databinding.FragmentLandingDBinding
 
 class LandingFragmentD : Fragment() {
 
-        override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landing_d, container, false)
+
+    return inflater.inflate(R.layout.fragment_landing_d, container, false)
+
     }
+
 
 //    val binding by lazy {FragmentLandingDBinding.inflate(layoutInflater)}
 //    binding.btnStart.setOnClickListener {
--- lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_Start/LandingViewPage.kt
+++ lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/A_Start/LandingViewPage.kt
@@ -1,6 +1,7 @@
 package com.example.lifeteer_ex1.A_Start
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,14 +11,16 @@ import com.example.lifeteer_ex1.LandingFragmentA
 import com.example.lifeteer_ex1.LandingFragmentB
 import com.example.lifeteer_ex1.LandingFragmentC
 import com.example.lifeteer_ex1.databinding.ActivityLandingViewPageBinding
+import com.example.lifeteer_ex1.databinding.ActivityLoginBinding
+import com.example.lifeteer_ex1.databinding.ActivityMainBinding
+import com.example.lifeteer_ex1.databinding.FragmentLandingDBinding
 
 class LandingViewPage : AppCompatActivity() {
 
     val binding by lazy {ActivityLandingViewPageBinding.inflate(layoutInflater)}
+    val binding2 by lazy {FragmentLandingDBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // val binding으로 하는것과 lazy나 latinent를 사용해서 하는 것의 차이는?
         // 1. 페이지 데이터 로드
         val list = listOf(LandingFragmentA(), LandingFragmentB(), LandingFragmentC(), LandingFragmentD())
         // 2. adaptor 생성
@@ -26,10 +29,18 @@ class LandingViewPage : AppCompatActivity() {
         binding.viewPager.adapter = pagerAdapter
         setContentView(binding.root)
 
+        val intent = Intent(this, LandingPage::class.java)
+        binding.btnBack.setOnClickListener {startActivity(intent)}
+        //뒤로가기 버튼 입력 시, 랜딩페이지 0번째 페이지로 이동
+
+
+
+        val intent2 = Intent(this, ActivityMainBinding::class.java)
+        binding2.fragDBtnStart.setOnClickListener{startActivity(intent2)}
+        //Fragment에 있는 마음짓기 시작하기 버튼으로 다음 액티비티로 넘어가야 하는데 이부분에서 문제가 계속 생김
 
     }
 }
-
 class FragmentPagerAdapter(val fragmentList:List<Fragment>, fragmentActivity: FragmentActivity)
     : FragmentStateAdapter(fragmentActivity){
 
