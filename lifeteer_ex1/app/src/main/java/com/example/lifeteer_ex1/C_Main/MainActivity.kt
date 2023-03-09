e29ae5bb7fc3437ee74b6c1153736b521029a840
Jong <jm991014@gmail.com>
2023-03-09 23:18:23 +0900

    메인페이지 추가 디자인
    .

--- lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/C_Main/MainActivity.kt
+++ lifeteer_ex1/app/src/main/java/com/example/lifeteer_ex1/C_Main/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.lifeteer_ex1.C_Main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.lifeteer_ex1.R
+import com.example.lifeteer_ex1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        replaceFragment(FragmentMindset())
+
+        binding.bnvMain.setOnItemSelectedListener { MenuItem ->
+            when (MenuItem.itemId) {
+                R.id.mindset -> replaceFragment(FragmentMindset())
+                R.id.diary -> replaceFragment(FragmentDiary())
+                R.id.recorded -> replaceFragment(FragmentRecorded())
+                R.id.mypage -> replaceFragment(FragmentMypage())
+            }
+            true
+        }    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        Log.d("MainActivity", "${fragment}")
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.frame_main, fragment)
+                commit()
+            }
     }
 }
